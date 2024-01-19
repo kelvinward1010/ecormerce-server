8fabe0203acb367e502c713ed32100b2e1b24fb9
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-20 00:10:49 +0700

    update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 start server: uvicorn app.main:app --reload
 
+uvicorn app.main:app --host 127.0.0.1 --port 1010
+
 pip install -r requirements.txt
 
 pip3 freeze > requirements.txt  # Python3
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from .routes import user, auth, admin
+import uvicorn
 
 app = FastAPI()
 
@@ -14,6 +15,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/", tags=["Welcome to API Server"], response_class=HTMLResponse)
 def welcome():
