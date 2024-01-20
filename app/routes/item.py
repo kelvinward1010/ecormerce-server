533f6b119ec4dde1868cefecec5f693ac371ea75
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-20 16:40:59 +0700

    update

--- app/routes/item.py
+++ app/routes/item.py
@@ -19,8 +19,8 @@ async def get_search_items(search):
     items_query_title = schemas.list_items(database.collection_items.find(myquery_name))
     return items_query_title
 
-@router.get("/find_post/{id}")
-async def find_post(id):
+@router.get("/find_item/{id}")
+async def find_item(id):
     item = database.collection_items.find_one({"_id": ObjectId(id)})
     if not item:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not found post with id: {id}")
